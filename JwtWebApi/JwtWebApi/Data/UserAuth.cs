fa2c35c3ffab617c0b9859d08497697aadad81d0
kkrasnoslobodtsev <90769620+lyrics0red@users.noreply.github.com>
2023-05-29 22:16:50 +0300

    Delete JwtWebApi directory

--- JwtWebApi/JwtWebApi/Data/UserAuth.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JwtWebApi
-{
-    public class UserAuth
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
